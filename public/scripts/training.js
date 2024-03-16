fe6b35b4042070c1fe7bfe2e70f2db84f4481184
GianniMS <1008999@hr.nl>
2024-03-16 14:05:27 +0100

    new trained model including idle pose

--- public/scripts/training.js
+++ public/scripts/training.js
@@ -24,7 +24,7 @@ const createHandLandmarker = async () => {
             delegate: "GPU"
         },
         runningMode: runningMode,
-        numHands: 2
+        numHands: 1
     });
 };
 createHandLandmarker();
